6c990329ee5d80c90a83f6924f64f296abcd33f0
Dylan-Bello <66900945+Dylan-Bello@users.noreply.github.com>
2020-10-24 14:34:44 +0800

    Merged

--- Nuclear Dragon/Assets/Scripts/Bullet.cs
+++ Nuclear Dragon/Assets/Scripts/Bullet.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float bulletSpeed = 0.1f; 
+    public float bulletSpeed = 1f; 
     public int damageToGive;
 
 
     void Update()
     {
         Vector3 pos = transform.position;
-        Vector3 velocity = new Vector3(0* Time.deltaTime, bulletSpeed);
+        Vector3 velocity = new Vector3(0 * Time.deltaTime, bulletSpeed);
         pos += transform.rotation * velocity;
         transform.position = pos;
     }
