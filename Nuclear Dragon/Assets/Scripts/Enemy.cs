7479f1c39637f24aa2a48adf9a631cbde7cd692f
Dylan-Bello <66900945+Dylan-Bello@users.noreply.github.com>
2020-11-02 14:34:55 +0800

    PlayTest Master

--- Nuclear Dragon/Assets/Scripts/Enemy.cs
+++ Nuclear Dragon/Assets/Scripts/Enemy.cs
@@ -9,6 +9,7 @@ public class Enemy : MonoBehaviour
     public float stopDistance;
 
     public Transform target;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +28,22 @@ public class Enemy : MonoBehaviour
             Chase();
         }
 
-        
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Vector3 rotation = new Vector3(0, 0, angle);
+
+        UpdateRotation(rotation);
     }
 
     void Chase()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
+
+    void UpdateRotation(Vector3 angle)
+    {
+        Quaternion rotation = Quaternion.Euler(angle);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, rotation, turnSpeed * Time.deltaTime);
     }
 
 }
